de15b621a89a57a36aa456326ff9124266cecb9e
Jeconiah Yohanes <jeconiahyohanes34@gmail.com>
2020-08-07 15:33:58 +0700

    Add listing all articles page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,10 @@
 class ArticlesController < ApplicationController
-  def show
+  def show # show => display individual article as requested
     @article = Article.find(params[:id]) #find by id
     # @variable is an instance variable supaya bisa dipake di html.erb
+  end
+
+  def index # index => display all articles available
+    @articles = Article.all
   end
 end
