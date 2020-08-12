9894a27bf752c6e0aba4900812f6b0beab6ad9e2
Jeconiah Yohanes <jeconiahyohanes34@gmail.com>
2020-08-12 11:35:47 +0700

    Users show and index page done, layout cleaned up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
   end
 
   def index
+    @users = User.all
   end
 
   def new
@@ -20,7 +21,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       flash[:notice] = "Welcome to Alpha Blog, #{@user.username}! You have successfully signed up!"
-      redirect_to articles_path
+      redirect_to @user
     else
       render 'new'
     end
@@ -30,7 +31,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "Your account information was successfully updated"
-      redirect_to articles_path
+      redirect_to @user
     else
       render 'edit'
     end
