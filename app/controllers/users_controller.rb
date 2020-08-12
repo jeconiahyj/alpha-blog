4f2b471e275103c53b6d66c272fa8c88f08eeb30
Jeconiah Yohanes <jeconiahyohanes34@gmail.com>
2020-08-12 10:49:39 +0700

    Added users show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,12 @@
 class UsersController < ApplicationController
+  
+  def show
+    @user = User.find(params[:id])
+    @articles = @user.articles
+  end
+
+  def index
+  end
 
   def new
     @user = User.new
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  
+  def gravatar_for(user, options = { size: 80 }) # helper method that can be used in views
+    # default for size is 80, but can be overriden
+    email_address = user.email.downcase
+    hash = Digest::MD5.hexdigest(email_address)
+    size = options[:size]
+    gravatar_url = "https://www.gravatar.com/avatar/#{hash}?s=#{size}"
+    image_tag(gravatar_url, alt: user.username, class: "rounded shadow mx-auto d-block")
+  end
+
 end
